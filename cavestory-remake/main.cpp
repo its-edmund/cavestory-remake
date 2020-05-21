c4b70d874351c97aa5f09b2483a025164304bfe7
Edmund Xin <mxinburritos@gmail.com>
2020-05-21 15:29:09 -0700

    Graphics added

--- cavestory-remake/main.cpp
+++ cavestory-remake/main.cpp
@@ -1,9 +1,18 @@
+/* Main.cpp
+ *This entry point of the program
+ */
+
 #include <iostream>
 #include <SDL.h>
 
+#include "graphics.h"
+
 using namespace std;
 
-int main(int argc, char *argv[]) {
-	cout << "Hello World!";
+int main(int argc, char* argv[]) {
+	Graphics graphics;
+	while (true) {
+
+	}
 	return 0;
 }
